10617da126a1682c3fd005ca1dc61ef48a208012
xrlin <xrlin@m.scnu.edu.cn>
2017-09-03 15:17:18 +0800

    Commit source files.

---
+++ convert.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func convert(f io.ReadCloser, chars []string, subWidth, subHeight int, imageSwitch bool, bgColor, penColor color.RGBA) (string, *image.NRGBA, error) {
+	defer f.Close()
+	var charsLength int = len(chars)
+	if charsLength == 0 {
+		return "", nil, fmt.Errorf("No chars provided")
+	}
+	if subWidth == 0 || subHeight == 0 {
+		return "", nil, fmt.Errorf("subWidth and subHeight params is required")
+	}
+	m, _, err := image.Decode(f)
+	if err != nil {
+		return "", nil, err
+	}
+	imageWidth, imageHeight := m.Bounds().Max.X, m.Bounds().Max.Y
+	var img *image.NRGBA
+	if imageSwitch {
+		img = initImage(imageWidth, imageHeight, bgColor)
+	}
+	piecesX, piecesY := imageWidth/subWidth, imageHeight/subHeight
+	var buff bytes.Buffer
+	for y := 0; y < piecesY; y++ {
+		offsetY := y * subHeight
+		for x := 0; x < piecesX; x++ {
+			offsetX := x * subWidth
+			averageBrightness := calculateAverageBrightness(m, image.Rect(offsetX, offsetY, offsetX+subWidth, offsetY+subHeight))
+			char := getCharByBrightness(chars, averageBrightness)
+			buff.WriteString(char)
+			if img != nil {
+				addCharToImage(img, char, x*subWidth, y*subHeight, penColor)
+			}
+		}
+		buff.WriteString("\n")
+	}
+	return buff.String(), img, nil
+}
+
+func initImage(width, height int, bgColor color.RGBA) *image.NRGBA {
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, bgColor)
+		}
+	}
+	return img
+}
+
+func calculateAverageBrightness(img image.Image, rect image.Rectangle) float64 {
+	var averageBrightness float64
+	width, height := rect.Max.X-rect.Min.X, rect.Max.Y-rect.Min.Y
+	for x := rect.Min.X; x < rect.Max.X; x++ {
+		for y := rect.Min.Y; y < rect.Max.Y; y++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			brightness := 0.299*float64(r>>8) + 0.587*float64(g>>8) + 0.114*float64(b>>8)
+			averageBrightness += brightness
+		}
+	}
+	averageBrightness /= float64(width * height)
+	return averageBrightness
+}
+
+func getCharByBrightness(chars []string, brightness float64) string {
+	index := int(brightness * float64(len(chars)) / 256)
+	if index == len(chars) {
+		index--
+	}
+	return chars[len(chars)-index-1]
+}
+
+func addCharToImage(img *image.NRGBA, char string, x, y int, penColor color.RGBA) {
+	face := basicfont.Face7x13
+	point := fixed.Point26_6{X: fixed.Int26_6(x * 64), Y: fixed.Int26_6(y * 64)}
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(penColor),
+		Face: face,
+		Dot:  point,
+	}
+	d.DrawString(char)
+}
+
+func getDataFromUrl(imageUrl string) (io.ReadCloser, error) {
+	var respImgData *http.Response
+	respImgData, err := http.Get(imageUrl)
+	if err != nil {
+		return nil, err
+	}
+	return respImgData.Body, nil
+}
+
+var colors map[string]color.RGBA = map[string]color.RGBA{"black": {0, 0, 0, 255},
+	"gray":  {128, 128, 128, 255},
+	"red":   {255, 0, 0, 255},
+	"green": {0, 128, 0, 255},
+	"blue":  {0, 0, 255, 255}}
+
+func main() {
+	server := flag.Bool("server", false, "Set true to enable web ui server")
+	bind := flag.String("ip", "127.0.0.1:8080", "The address to bind.")
+	imagePath := flag.String("image-path", "", "The path of the picture file(jpg/png).")
+	imageUrl := flag.String("image-url", "", "The url of the picture file(jpg/png).")
+	characters := flag.String("characters", "M80V1i:*|, ", "The chars in the value will be used to generate ascii art.")
+	subWidth := flag.Int("sub-width", 10, "The width of the piece of small rectangle")
+	subHeight := flag.Int("sub-height", 10, "The height of the piece of small rectangle")
+	imageOut := flag.Bool("image-out", false, "If set to true, will also output the ascii art to an image file")
+	imageOutPath := flag.String("image-out-path", "", "If imageOut is true, add this option to print the picture to file.")
+	bgColorType := flag.String("bg", "black", "The background of the ascii art image file.(Only useful when image-out is true, red|gray|green|blue|black are available).")
+	penColorType := flag.String("color", "gray", "The color of the ascii font in image file.(Only useful when image-out is true, red|gray|green|blue|black are available).")
+	flag.Parse()
+
+	if *server {
+		startServer(*bind)
+	}
+
+	var file io.ReadCloser
+	var err error
+	if *imagePath != "" {
+		file, err = os.Open(*imagePath)
+	} else {
+		file, err = getDataFromUrl(*imageUrl)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	//chars := []string{"M", "8", "0", "V", "1", "i", ":", "*", "|", ".", " "}
+	chars := strings.Split(*characters, "")
+	bgColor, penColor := colors[*bgColorType], colors[*penColorType]
+	result, img, err := convert(file, chars, *subWidth, *subHeight, *imageOut, bgColor, penColor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if img != nil {
+		imgFile, _ := os.Create(*imageOutPath)
+		defer imgFile.Close()
+		//img = imaging.Fit(img, 800, 600, imaging.Lanczos)
+		png.Encode(imgFile, img)
+	}
+	fmt.Print(result)
+}
---
+++ server.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	data, _ := Asset("index.html")
+	w.Write(data)
+}
+
+type config struct {
+	ImageLink string `json:"image_link"`
+}
+
+type serverError struct {
+	Error string `json:"error"`
+}
+
+type responseData struct {
+	Ascii string `json:"ascii"`
+	Image string `json:"image"`
+}
+
+func ascii(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(0)
+	image_link := r.FormValue("image_link")
+	imageFile, _, err := r.FormFile("image_file")
+	characters := strings.Split(r.FormValue("characters"), "")
+	subWidth, err := strconv.Atoi(r.FormValue("sub_width"))
+	if err != nil {
+		errorMessage, _ := json.Marshal(serverError{Error: err.Error()})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(errorMessage)
+		return
+	}
+	subHeight, err := strconv.Atoi(r.FormValue("sub_height"))
+	if err != nil {
+		errorMessage, _ := json.Marshal(serverError{Error: err.Error()})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(errorMessage)
+		return
+	}
+	bgColor := r.FormValue("bg")
+	penColor := r.FormValue("pen")
+	defer func() {
+		if imageFile != nil {
+			imageFile.Close()
+		}
+	}()
+	var imgData io.ReadCloser
+	if err == nil && imageFile != nil {
+		imgData = io.ReadCloser(imageFile)
+	} else {
+		var respImgData *http.Response
+		fmt.Println(image_link)
+		respImgData, err = http.Get(image_link)
+		if err != nil {
+			errorMessage, _ := json.Marshal(serverError{Error: err.Error()})
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(errorMessage)
+			return
+		}
+		imgData = respImgData.Body
+	}
+	if err != nil {
+		errorMessage, _ := json.Marshal(serverError{Error: err.Error()})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(errorMessage)
+		return
+	}
+	result, img, err := convert(imgData, characters, subWidth, subHeight, true, colors[bgColor], colors[penColor])
+	if err != nil {
+		errorMessage, _ := json.Marshal(serverError{Error: err.Error()})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(errorMessage)
+		return
+	}
+	imgBase64, err := imageToBase64(img)
+	if err != nil {
+		errorMessage, _ := json.Marshal(serverError{Error: err.Error()})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(errorMessage)
+		return
+	}
+	resp, _ := json.Marshal(responseData{Ascii: result, Image: imgBase64})
+	w.Write(resp)
+}
+
+func imageToBase64(img *image.NRGBA) (string, error) {
+	buffer := bytes.NewBuffer(nil)
+	err := png.Encode(buffer, img)
+	if err != nil {
+		return "", err
+	}
+	result := base64.StdEncoding.EncodeToString(buffer.Bytes())
+	result = "data:image/png;base64, " + result
+	return result, nil
+}
+
+func startServer(addr string) {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/ascii", ascii)
+	http.ListenAndServe(addr, nil)
+}
